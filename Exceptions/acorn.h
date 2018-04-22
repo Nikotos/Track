add4cf880b4e3e0a07dff965a78ab0a07b50905f
Dubovik Nikita <bup1350@gmail.com>
2018-04-22 22:59:40 +0000

    Add files via upload

--- Exceptions/acorn.h
+++ Exceptions/acorn.h
@@ -1,24 +1,37 @@
 #ifndef ACORN_H
 #define ACORN_H
 
+#include <stdio.h>
 /*------------------------------------------------------------------------
+ *                                                  The newest version!
  *
  *  Exception class "Acorn"
  *  Acorn - easy and nicely to throw
  *  Acorn - full range of information will thrown directly "on yor head"
  *
  /------------------------------------------------------------------------*/
+#define THROW(ERRCODE, MESSAGE, POINTER)\
+    throw new Acorn(ERRCODE, MESSAGE , __PRETTY_FUNCTION__,__FILE__,__LINE__, POINTER)
+
+#define FTHROW(ERRCODE, MESSAGE)\
+    throw new Acorn(ERRCODE, MESSAGE , __PRETTY_FUNCTION__,__FILE__,__LINE__, nullptr)
+
 enum ERRORS {
     MEMORY_ALLOCATION_ERROR = 0,
     MEMORY_ACCES_ERROR,
     INVALID_ARGUMENT,
     OUT_OF_RANGE,
     LOGIC_ERROR,
+    SYNTAX_ERROR,
+    QUEUE_OVERFLOW,
     UNKNOWN_ERROR = 42
 };
 
-class Acorn {
-public:
+const char* LOG_FILE = "acorn_log.txt";
+
+
+struct Acorn
+{
     enum ERRORS errcode_ = UNKNOWN_ERROR;
     const char* message_;
     const char* function_name_;
@@ -33,9 +46,9 @@ public:
           int line,
           Acorn* Acorn_pointer);
 
-    inline void write_log(const char* LOG_FILE_NAME);
+    void write_log(const char* LOG_FILE_NAME);
 
-    inline void write_log();
+    void write_log();
 
     void tell_user();
 };
@@ -53,10 +66,10 @@ inline Acorn::Acorn(enum ERRORS errcode,
         line_(line),
         Acorn_pointer_(Acorn_pointer)
 {
-    write_log("acorn_log.txt");
+    write_log(LOG_FILE);
 }
 
-inline void Acorn::write_log(const char *LOG_FILE_NAME)
+void Acorn::write_log(const char *LOG_FILE_NAME)
 {
     FILE* log = fopen(LOG_FILE_NAME, "a");
 
@@ -68,11 +81,11 @@ inline void Acorn::write_log(const char *LOG_FILE_NAME)
                      "Error code is [%d]\n"
                      "Message is [%s] \n",
                      acorn->file_name_,
-                     acorn->function_name_, 
+                     acorn->function_name_,
                      acorn->line_,
                      acorn->errcode_,
                      acorn->message_);
-        
+
         if (acorn->Acorn_pointer_ != nullptr) {
             fprintf(log, "More Info.....\n\n");
         }
@@ -80,12 +93,12 @@ inline void Acorn::write_log(const char *LOG_FILE_NAME)
     }
 }
 
-inline void Acorn::write_log()
+void Acorn::write_log()
 {
-    write_log("acorn_log.txt");
+    write_log(LOG_FILE);
 }
 
-inline void Acorn::tell_user() {
+void Acorn::tell_user() {
     Acorn* acorn = this;
     while(acorn != nullptr) {
         printf("Error in FILE [%s]\n"
--- Exceptions/main.cpp
+++ Exceptions/main.cpp
@@ -24,42 +24,56 @@ void surface_function(){
     catch(Acorn* acorn)
     {
         /* Here may be your condition*/
-        if (1 == 1) {
-            throw new Acorn(MEMORY_ACCES_ERROR, "I dont know, whar happend, but it may be serious", __PRETTY_FUNCTION__, __FILE__, __LINE__,acorn);
+        if (1 == 1)
+        {
+            THROW(MEMORY_ACCES_ERROR,"I dont know, whar happend, but it may be serious",acorn);
         }
     }
 }
 
-void under_surface_function() {
+void under_surface_function()
+{
     try {
         deep_function();
     }
     catch(Acorn* acorn)
     {
-        if (true == 1) {
-            throw new Acorn(LOGIC_ERROR, "some troubels donwstairs, I dont care", __PRETTY_FUNCTION__, __FILE__, __LINE__, acorn);
+        /* Here may be your condition*/
+        if (true == 1)
+        {
+            THROW(LOGIC_ERROR, "some troubels donwstairs, I dont care", acorn);
         }
     }
 };
 
-void deep_function() {
+void deep_function()
+{
     try {
         the_deepest_function();
     }
     catch(Acorn* acorn)
     {
-        if (true == true) {
-            throw new Acorn(LOGIC_ERROR, "FALSE IS TRUE, WHAT!?", __PRETTY_FUNCTION__, __FILE__, __LINE__,acorn);
+        if (true == true)
+        {
+            THROW(LOGIC_ERROR, "FALSE IS TRUE, WHAT!?", acorn);
         }
     }
 }
 
+
+
 /* simulation of unexpected error*/
 #define false true
-void the_deepest_function(){
-    if (false){
-        throw new Acorn(UNKNOWN_ERROR, "FALSE IS TRUE, ZAPUSKAEM GUSYA, RABOTYAGI", __PRETTY_FUNCTION__, __FILE__, __LINE__, nullptr);
+
+
+void the_deepest_function()
+{
+    if (false)
+    {
+        FTHROW(UNKNOWN_ERROR,"FALSE IS TRUE, ZAPUSKAEM GUSYA, RABOTYAGI");
     }
 
 }
+
+
 #undef false
